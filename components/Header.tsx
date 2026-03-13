e242327bb72b6c0619ed52617fc23f1c70d2159a
Rahul <rahul.altudo@gmail.com>
2026-03-13 10:08:53 +0530

    changes offer at top

--- components/Header.tsx
+++ components/Header.tsx
@@ -28,10 +28,10 @@ const navItems = [
     name: "Offers",
     to: "/buy-one-get-one-free",
     submenu: [
+      { name: "Eid Special Offer", to: "/eid-offers", price: "899" },
       { name: "Buy 1 (100ml) Get Free 1 Gift", to: "/buy-one-get-one-free", price: "399" },
       { name: "Buy 2 (100ml) Get Free 4 Gifts", to: "/buy-two-get-free", price: "899" },
       { name: "Buy 3 (100ml) Get Free 5 Gifts", to: "/buy-three-get-gifts", price: "1299" },
-      { name: "Eid Special Offer", to: "/eid-offers", price: "899" },
     ]
   },
   {
